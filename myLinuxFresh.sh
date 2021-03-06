34cd71cbfb6d690898d7269e5edc7112a7b927f0
Anmoldeep1509 <anmoldeep1509@gmail.com>
2021-03-07 03:04:13 +0530

    Added Utility Installation Script

---
+++ myLinuxFresh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# USE WITH ROOT PRIVILEGES!
+apt update
+apt upgrade -y
+
+# Utility - nvbn/thefuck
+apt update
+apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck
+
+# Node Version manager install - nvm-sh/nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+# Youtube-DL
+pip3 install --upgrade youtube_dl
