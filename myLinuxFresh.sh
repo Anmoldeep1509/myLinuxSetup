835d17ae9ebb1f71bde4a0ce194055895ff92417
Anmoldeep Singh Arora <31613431+Anmoldeep1509@users.noreply.github.com>
2021-04-24 07:42:58 +0100

    added prompt flags

--- myLinuxFresh.sh
+++ myLinuxFresh.sh
@@ -6,11 +6,12 @@ apt upgrade -y
 
 # Utility - nvbn/thefuck
 apt update
-apt install python3-dev python3-pip python3-setuptools
+apt install python3-dev python3-pip python3-setuptools -y
 pip3 install thefuck
 
 # Node Version manager install - nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # Youtube-DL
-pip3 install --upgrade youtube_dl
+pip3 install --upgrade youtube_dl -y
+
